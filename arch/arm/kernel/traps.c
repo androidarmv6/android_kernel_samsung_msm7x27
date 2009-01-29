d79cc602738fe8b29f17dcdb26106b98914f14cb
Brian Swetland <swetland@google.com>
2009-01-28 20:45:48 -0800

    HACK stay compatible with the emulated TLS register HACK
    psyke83: wrap hack into CONFIG_TLS_USERSPACE_EMUL option
    
    Signed-off-by: Pankaj <pankaj.bharadiya@ti.com>
    
    Change-Id: I24c45b5e0390fe2c56c2f810ca380b32a389a7d9

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -531,7 +531,11 @@ asmlinkage int arm_syscall(int no, struct pt_regs *regs)
 		if (has_tls_reg) {
 			asm ("mcr p15, 0, %0, c13, c0, 3"
 				: : "r" (regs->ARM_r0));
-		} else {
+		}
+#ifndef CONFIG_TLS_USERSPACE_EMUL
+		else
+#endif
+		{
 			/*
 			 * User space must never try to access this directly.
 			 * Expect your app to break eventually if you do so.
