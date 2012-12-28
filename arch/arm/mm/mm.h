130743f9338ff29ab8cbdd883f02737accd0a6bf
erikcas <erikcas1972@gmail.com>
2012-12-28 14:27:52 +0100

    Revert "msm: pmem: Squashed update to version from msm 2.6.35 kernel"
    This reverts commit f154c850bf9383c85f5634450e23ecda63544390.

--- arch/arm/mm/mm.h
+++ arch/arm/mm/mm.h
@@ -15,7 +15,13 @@ static inline pmd_t *pmd_off_k(unsigned long virt)
 	return pmd_off(pgd_offset_k(virt), virt);
 }
 
-struct mem_type;
+struct mem_type {
+	unsigned int prot_pte;
+	unsigned int prot_l1;
+	unsigned int prot_sect;
+	unsigned int domain;
+};
+
 const struct mem_type *get_mem_type(unsigned int type);
 
 extern void __flush_dcache_page(struct address_space *mapping, struct page *page);
